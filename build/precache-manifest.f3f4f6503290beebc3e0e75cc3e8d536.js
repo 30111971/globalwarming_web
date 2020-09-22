210466aa7ba02509c5a9f1a83a4016c434470627
30111971 <matheusmaica@gmail.com>
2020-09-21 23:02:15 -0300

    sdgfdg

---
+++ build/precache-manifest.f3f4f6503290beebc3e0e75cc3e8d536.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506653de439806a803891428b3976460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a580b1c04403ce45c77",
+    "url": "/static/css/main.bba00fa8.chunk.css"
+  },
+  {
+    "revision": "1ba58abb3388e92f6514",
+    "url": "/static/js/2.1222af0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1222af0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a580b1c04403ce45c77",
+    "url": "/static/js/main.8381ee15.chunk.js"
+  },
+  {
+    "revision": "378cfb5dd85c4833fe46",
+    "url": "/static/js/runtime-main.8c138f6f.js"
+  },
+  {
+    "revision": "714f03e92818da4bfe9266ed24b7a570",
+    "url": "/static/media/historicalCO2Level.714f03e9.png"
+  },
+  {
+    "revision": "f83dac48b96e5d2775049bb4ef7c66ef",
+    "url": "/static/media/petroliferas.f83dac48.png"
+  },
+  {
+    "revision": "504b9587603bf661f8a255ab575a5ca8",
+    "url": "/static/media/seaLevelTemp.504b9587.png"
+  },
+  {
+    "revision": "6da4830c165a24268cbfecfea0e307bc",
+    "url": "/static/media/tabela.6da4830c.png"
+  },
+  {
+    "revision": "aa7541701527d02a44593ee0f38dcbf3",
+    "url": "/static/media/tempCO2Graph.aa754170.png"
+  },
+  {
+    "revision": "b0c152c0ec5e828389afe526cf7e5a18",
+    "url": "/static/media/temperatureOfThePlanet.b0c152c0.png"
+  },
+  {
+    "revision": "f031e797cd6ae020f5b07600ac97886c",
+    "url": "/static/media/worldPopulation.f031e797.png"
+  },
+  {
+    "revision": "5c2d24261445fb4fcd85012f699eb4a7",
+    "url": "/static/media/worldPopvsCO2.5c2d2426.png"
+  }
+]);
